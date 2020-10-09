e98087795a319327ca4b95d6a9f5070e6f13c9aa
1410200695@qq.com <h41607521>
2020-10-09 17:54:11 +0800

    erlang base tutorial
    [+] 更新至递归调用

--- part3/erlang/src/recursion/list_reversal.erl
+++ part3/erlang/src/recursion/list_reversal.erl
@@ -2,7 +2,7 @@
 -author("Administrator").
 
 %% API
--export([]).
+-export([tail_reverse/1,tail_reverse/2,start/0]).
 tail_reverse(L) -> tail_reverse(L,[]).
 tail_reverse([],Acc) -> Acc;
 tail_reverse([H|T],Acc) -> tail_reverse(T, [H|Acc]).
