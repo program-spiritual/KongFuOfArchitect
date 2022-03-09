0d99d352054eeac6d53b6c4e8840a3fbea77f1ef
hhy <huhongyun@ykccn.com>
2022-03-09 15:06:41 +0800

    chore: add xamarin demo1 , latter , i could catch up readme file

---
+++ part13/learn-xamarin/App2/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Content;
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button button  = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += delegate { button.Text = "Hello, this is your first app"; };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
