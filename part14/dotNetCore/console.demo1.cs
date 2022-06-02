45f2ce586cc76da65af4c3b86e296c1ed1c1a001
xiaosanye <xiaosanye@daomubiji.com>
2022-06-02 21:51:15 +0800

    chore: update more doc about dot net core , and i would write some code under the dot Net directory intermittently。

---
+++ part14/dotNetCore/console.demo1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console.demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
