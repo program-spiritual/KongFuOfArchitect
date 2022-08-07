a22405ca2ed458448f5f29e0d2384d982386fb91
xiaosanye <xiaosanye@daomubiji.com>
2022-08-07 23:01:37 +0800

    chore: part3 add call by value and call by ref pyton files.

---
+++ part3/functionalProgramming/call_by_ref.py
@@ -0,0 +1,18 @@
+def swap(a, b):
+    t = a
+    a = b
+    b = t
+    print("value of a inside the function: :", a)
+    print("value of b inside the function: ", b)
+    return (a, b)
+
+
+if __name__ == '__main__':
+    # Now we can call swap function
+    a = 50
+    b = 75
+    print("value of a before sending to function: ", a)
+    print("value of b before sending to function: ", b)
+    x = swap(a, b)
+    print("value of a after sending to function: ", x[0])
+    print("value of b after sending to function: ", x[1])
