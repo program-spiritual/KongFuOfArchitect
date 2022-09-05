2720b375cf590b2efb4b1fc63dba52b0165893fd
xiaosanye <xiaosanye@daomubiji.com>
2022-09-06 03:53:25 +0800

    chore: add erlang number atom data type example code.

---
+++ part3/functionalProgramming/er_functional/src/atomic_string.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author xiaosanye
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. 九月 2022 02:28
+%%%-------------------------------------------------------------------
+-module(atomic_string).
+-author("xiaosanye").
+-export([start/0]).
+
+start()->
+  io:fwrite(monday).
+%% API
